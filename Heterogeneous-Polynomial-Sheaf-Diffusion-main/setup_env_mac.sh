018a213c49c397128c70ba84b0af2ad520f5f445
JLloyd22 <157820348+JLloyd22@users.noreply.github.com>
2026-03-03 17:31:46 +0000

    Delete Heterogeneous-Polynomial-Sheaf-Diffusion-main directory

--- Heterogeneous-Polynomial-Sheaf-Diffusion-main/setup_env_mac.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/zsh
-#source this file to setup the environment variables for macOS
-export LDFLAGS="${LDFLAGS/--no-as-needed/}"
-uv sync
-export PYTHONPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd):$PYTHONPATH"
--- Heterogeneous-Polynomial-Sheaf-Diffusion-main/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-#  Copyright (c) 2024. Luke Braithwaite
-#  License: MIT
